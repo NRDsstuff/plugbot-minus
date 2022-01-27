5a1d13d0c3c48fa2bbcec50b17e7f27bc5ac176d
NRD <85891552+NRDsstuff@users.noreply.github.com>
2022-01-27 10:59:04 +0100

    Update install.js

--- install.js
+++ install.js
@@ -36,7 +36,6 @@ module.exports = (rl, user) =>{
         if(err) console.log("there was an error while installing the plugbot toolkit.")
     })
     NZTK.moveFile("./SHELL/temp/NZPM/plugbot/other/PBTK.js", "./SHELL/other/PBTK.js")
-    NZTK.removedir('./SHELL/temp/NZPM/plugbot')
 
     fs.appendFile('./SHELL/configs/NZPM/installed.txt', "plugbot", (err) =>{
 
