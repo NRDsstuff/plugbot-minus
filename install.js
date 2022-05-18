3773bb37f08d8a33e31e51247a83789a0092d107
NRDeez <nrdfoss@gmail.com>
2022-05-18 08:07:32 +0200

    hopefully fixed the installer

--- install.js
+++ install.js
@@ -1,12 +1,10 @@
-const { ShellString } = require('shelljs')
-
-module.exports = (rl, user) =>{
+module.exports = (rl, user, cb) =>{
 
     const fs = require('fs')
     const shell = require('shelljs')
     const NZTKc = require('../../../other/NZTK')
     const NZTK = new NZTKc("NZPM", user)
-    let installed = require('../../../configs/NZPM/toupdate.json')
+    let installed = require('../../configs/NZPM/toupdate.json')
 
     // set up a readline for init config
 
@@ -19,33 +17,21 @@ module.exports = (rl, user) =>{
         output: process.stdout
     })
 
-    shell.exec("npm i discord.js db discord.js-reaction-menu one-liner-joke superagent ffmpeg @discordjs/rest @discordjs/builders discord-api-types")
+    // takes way too long to do idk why
 
-    PBRL.question(`please input your bot's token (it will be set for the current user, ${user}.)`, (answer) =>{
+    PBRL.question(`please input your bot's token (it will be set for the current user, ${user.name}.)`, (answer) =>{
 
-        fs.appendFile(`./SHELL/temp/NZPM/plugbot/configs/plugbot/tokens.json`, `{"${user}": "${answer}"}`, (err) =>{
+        fs.appendFile(`./SHELL/temp/NZPM/plugbot/configs/plugbot/tokens.json`, `{"${user.name}": "${answer}"}`, (err) =>{
 
-            if (err) console.log("there was an error while setting your token. you will have to add it manually in shell/configs/plugbot/tokens")
+            if (err) NZTK.log.error(`could not set the token automatically. you will have to set the token in SHELL/temp/NZPM/plugbot/configs/plugbot/tokens.json manually: {"MINUS SHELL USERNAME": TOKEN}`, 2, 'e')
         })
+
+        // move shtuff
 
         NZTK.moveFile('./SHELL/temp/NZPM/plugbot/programs/plugbot.app.js', './SHELL/programs/plugbot.app.js', "there was an error while moving the app file.", true)
-        fs.rename('./SHELL/temp/NZPM/plugbot/other/plugbot', './SHELL/other/plugbot', (err) =>{
-
-            if(err) console.log("there was an error while moving the commands")
-        })
-        fs.rename("./SHELL/temp/NZPM/plugbot/configs/plugbot", "./SHELL/configs/plugbot", (err) =>{
-
-            if(err) console.log("there was an error while moving config and token files")
-        })
-        fs.rename("./SHELL/temp/NZPM/plugbot/other/PBTKModules", "./SHELL/other/PBTKModules", (err) =>{
-
-            if(err) console.log("there was an error while installing the plugbot toolkit.")
-        })
-        fs.mkdir('./SHELL/other/plugbot/plugins', (err) =>{
-
-            if(err) console.log("there was an error while making the plugins folder")
-        })
-        NZTK.moveFile("./SHELL/temp/NZPM/plugbot/other/PBTK.js", "./SHELL/other/PBTK.js", " ", true)
+        NZTK.moveFile('./SHELL/temp/NZPM/plugbot/other/plugbot', './SHELL/other/plugbot', "there was an error while moving the misc functions.", true)
+        NZTK.moveFile("./SHELL/temp/NZPM/plugbot/other/PBTK.js", "./SHELL/other/PBTK.js", "there was an error while moving the PBTK core", true)
+        NZTK.moveFile('./SHELL/temp/NZPM/plugbot/other/PBTKModules', './SHELL/other/PBTKModules', "there was an error while moving the PBTK files.", true)
         
         if(!installed.packages.indexOf("plugbot")) installed.packages.push("plugbot")
 
@@ -54,7 +40,13 @@ module.exports = (rl, user) =>{
             if(err) console.log("there was an error while installing this package")
         }))
 
+        // node dependencies 
+
+        shell.exec("npm i discord.js db discord.js-reaction-menu one-liner-joke superagent ffmpeg @discordjs/rest @discordjs/builders discord-api-types")
+
+        // finishing up shtuff
+
         NZTK.removeDir('./SHELL/temp/NZPM/plugbot')
-        return NZTK.log.success("finished installing plugbot", 1, "install")
+        cb("plugbot")
     })
 }
